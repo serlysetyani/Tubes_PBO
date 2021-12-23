5f91285cc4e95acfccccf2f1cc408bcf737d81d3
Kevin <96371699+kdrm12@users.noreply.github.com>
2021-12-23 12:07:23 +0700

    Add Class Laundry

---
+++ src/Manajemen_Hotel/Laundry.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Manajemen_Hotel;
+
+/**
+ *
+ * @author A412FL
+ */
+public class Laundry {
+    private int harga_layanan, berat_per_kg;
+    
+    public String getLayanan_dipesan(){
+        return null; //Belum Tahu
+    }
+
+    public int getHarga_layanan() {
+        return harga_layanan;
+    }
+
+    public int getBerat_per_kg() {
+        return berat_per_kg;
+    }
+
+    public Laundry() {
+    }
+
+    public Laundry(int harga_layanan, int berat_per_kg) {
+        this.harga_layanan = harga_layanan;
+        this.berat_per_kg = berat_per_kg;
+    }
+    
+    
+}
