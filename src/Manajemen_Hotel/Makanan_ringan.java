659493feb06433631e948e557ceeb90ae5a2b1f4
Serly Setyani <sherly.setyani@gmail.com>
2021-12-23 17:16:32 +0700

    Update Makanan_ringan.java

--- src/Manajemen_Hotel/Makanan_ringan.java
+++ src/Manajemen_Hotel/Makanan_ringan.java
@@ -10,5 +10,26 @@ package Manajemen_Hotel;
  * @author SUYANTO
  */
 public class Makanan_ringan {
+    private String nama;
+    private int harga_layanan, jumlah;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getHarga_layanan() {
+        return harga_layanan;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public Makanan_ringan(String nama, int harga_layanan, int jumlah) {
+        this.nama = nama;
+        this.harga_layanan = harga_layanan;
+        this.jumlah = jumlah;
+    }
     
+    public Makanan_ringan(){}
 }
